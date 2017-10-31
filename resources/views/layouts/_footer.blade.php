a30212da97fc216238655ddbfe0cd8ab39ecfa5f
todd <hey_todd@foxmail.com>
2017-10-31 15:47:00 +0000

    models

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -2,7 +2,7 @@
   <footer class="footer">
     <small class="slogon">
       <img class="brand-icon" src="https://dn-phphub.qbox.me/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34">
-      <a href="https://yizhinangua.bujieshi.net">
+      <a href="http://yizhinangua.bujieshi.net">
         一只南瓜
       </a>
     </small>
