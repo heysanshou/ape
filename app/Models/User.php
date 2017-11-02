7f2f88a3106fcb14ec3293d2f23350221df83bf8
todd <hey_todd@foxmail.com>
2017-11-02 14:26:21 +0000

    singin

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+class User extends Authenticatable
 {
     //
     protected $fillable = ['name', 'email', 'password'];
