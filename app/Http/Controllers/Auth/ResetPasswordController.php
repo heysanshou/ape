d68af4effde50f4ce0b07b697a0d5d37eefa830f
todd <hey_todd@foxmail.com>
2017-11-02 17:43:46 +0000

    passwd

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Notifications\Notifiable;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+use App\Notifications\ResetPassword;
+
 class User extends Authenticatable
 {
     //
@@ -14,9 +16,25 @@ class User extends Authenticatable
 
     protected $hidden = ['password', 'remember_token'];
 
+    use Notifiable;
+
     public function gravatar($size = '128')
     {
         $rand = $this->id % 10;
         return "https://adminlte.io/themes/AdminLTE/dist/img/user{$rand}-{$size}x{$size}.jpg";
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($user) {
+            $user->activation_token = str_random(30);
+        });
+    }
+
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetPassword($token));
     }
 }
